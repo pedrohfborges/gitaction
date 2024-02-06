42ec797435e17e8857494fbd7d3fa79e5759cfaf
Pedro Henrique Ferreira Borges <pedrohfborges4@gmail.com>
2024-02-06 15:34:31 -0300

    alteracao

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 ####NET CONFIGURATION
 
 variable vpc {
-  default     = "192.168.0.0/16"
+  default     = "10.10.0.0/16"
   description = "Set VCN IP"
 }
 
 variable netpublic {
-  default     = "192.168.1.0/24"
+  default     = "10.10.1.0/24"
   description = "Set Subnet Public"
 }
 
 variable netprivate {
-  default     = "192.168.2.0/24"
+  default     = "10.10.2.0/24"
   description = "Set subnet Private"
 }
 
