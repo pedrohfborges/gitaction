9f09c5640559f7df47d7f6405db74d79314b823a
Pedro Henrique Ferreira Borges <pedrohfborges4@gmail.com>
2024-02-06 15:28:21 -0300

    alteracao

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,16 @@
-# resource "aws_instance" "app_server" {
-#   ami           = var.instanceami
-#   instance_type = var.instancetype
-#   # key_name = "chave-teste" #################COMANDO PARA CHAVE SSH######
-#   user_data =  <<-EOF
-#                   #!bin/bash
-#                   sudo apt install apache2 -y
-#                   cd /var/www/html/
-#                   echo "<h1> Pedro Henrique Ferreira Borges</h1>" > index.html
-#                   nohup busybox httpd -f -p 8080 &
-#                   EOF 
-#   tags = {
-#     Name = "Minha Primeira Imagem"
-#   }
-# }
+resource "aws_instance" "app_server" {
+  ami           = var.instanceami
+  instance_type = var.instancetype
+  # key_name = "chave-teste" #################COMANDO PARA CHAVE SSH######
+  # user_data =  <<-EOF
+  #                 #!bin/bash
+  #                 sudo apt install apache2 -y
+  #                 cd /var/www/html/
+  #                 echo "<h1> Pedro Henrique Ferreira Borges</h1>" > index.html
+  #                 nohup busybox httpd -f -p 8080 & 
+  #               EOF
+  host_resource_group_arn = "arn:aws:iam::159163223708:instance-profile/Fullacessec2"
+  tags = {
+    Name = "Minha Primeira Imagem"
+  }
+}
