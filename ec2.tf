e9d290ffdcbd57e651f2aae7042a292fd267f644
Pedro Henrique Ferreira Borges <pedrohfborges4@gmail.com>
2024-02-06 13:43:11 -0300

    commit

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,8 @@
-# resource "aws_instance" "web" {
-#   ami           = var.instance-ami
-#   instance_type = var.instance-type
-#   subnet_id     = aws_subnet.public_subnet.id
-#   associate_public_ip_address =  true
-#   key_name = "chave-ssh"
+resource "aws_instance" "web" {
+  ami           = var.instance-ami
+  instance_type = var.instance-type
+  subnet_id     = aws_subnet.public_subnet.id
+  associate_public_ip_address =  true
+  key_name = "chave-ssh"
 
-# }
-
-# resource "aws_instance" "web2" {
-#   instance_type = var.instance-type
-#   ami           = var.instance-ami
-#   key_name = "chave-ssh"
-# }
+}
