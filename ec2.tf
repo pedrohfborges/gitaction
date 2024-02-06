73708607266bbea1a5a19fb39348daf19bcc0107
Pedro Henrique Ferreira Borges <pedrohfborges4@gmail.com>
2024-02-06 15:31:47 -0300

    alteracao

--- ec2.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_instance" "web" {
-  ami           = var.instance-ami
-  instance_type = var.instance-type
-  host_resource_group_arn = "arn:aws:iam::159163223708:instance-profile/Fullacessec2"
-  key_name = "chave-ssh"
-}
