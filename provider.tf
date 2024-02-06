a2a4fa69e92575431c0dc9e6f25d640abd86fe48
Pedro Henrique Ferreira Borges <pedrohfborges4@gmail.com>
2024-02-06 11:50:23 -0300

    alterações

--- provider.tf
+++ provider.tf
@@ -1,15 +1,4 @@
 provider "aws" {
   region  = "us-east-2"
-  version = "~> 2.0"
+  version = "~> 2.0.3"
 }
-
-
-
-terraform {
-      required_providers {
-         aws = {
-         source = "hashicorp/aws"
-         version = "= 3.74.2"
-        }
-     }
-  }
