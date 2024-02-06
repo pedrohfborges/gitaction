f93f01bb64800b20a8a198558cbe9f348d82f8a5
Pedro Henrique Ferreira Borges <pedrohfborges4@gmail.com>
2024-02-06 15:30:21 -0300

    alteracao

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,6 @@
-resource "aws_instance" "app_server" {
-  ami           = var.instanceami
-  instance_type = var.instancetype
-  # key_name = "chave-teste" #################COMANDO PARA CHAVE SSH######
-  # user_data =  <<-EOF
-  #                 #!bin/bash
-  #                 sudo apt install apache2 -y
-  #                 cd /var/www/html/
-  #                 echo "<h1> Pedro Henrique Ferreira Borges</h1>" > index.html
-  #                 nohup busybox httpd -f -p 8080 & 
-  #               EOF
+resource "aws_instance" "web" {
+  ami           = var.instance-ami
+  instance_type = var.instance-type
   host_resource_group_arn = "arn:aws:iam::159163223708:instance-profile/Fullacessec2"
-  tags = {
-    Name = "Minha Primeira Imagem"
-  }
+  key_name = "chave-ssh"
 }
