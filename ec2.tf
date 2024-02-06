36ae7ae40a8754bddcefb850ec31ffb8f36c2172
Pedro Henrique Ferreira Borges <pedrohfborges4@gmail.com>
2024-02-06 14:35:15 -0300

    commit

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,15 @@
-resource "aws_instance" "app_server" {
-  ami           = var.instanceami
-  instance_type = var.instancetype
-  # key_name = "chave-teste" #################COMANDO PARA CHAVE SSH######
-  user_data =  <<-EOF
-                  #!bin/bash
-                  sudo apt install apache2 -y
-                  cd /var/www/html/
-                  echo "<h1> Pedro Henrique Ferreira Borges</h1>" > index.html
-                  nohup busybox httpd -f -p 8080 &
-                  EOF 
-  tags = {
-    Name = "Minha Primeira Imagem"
-  }
-}
+# resource "aws_instance" "app_server" {
+#   ami           = var.instanceami
+#   instance_type = var.instancetype
+#   # key_name = "chave-teste" #################COMANDO PARA CHAVE SSH######
+#   user_data =  <<-EOF
+#                   #!bin/bash
+#                   sudo apt install apache2 -y
+#                   cd /var/www/html/
+#                   echo "<h1> Pedro Henrique Ferreira Borges</h1>" > index.html
+#                   nohup busybox httpd -f -p 8080 &
+#                   EOF 
+#   tags = {
+#     Name = "Minha Primeira Imagem"
+#   }
+# }
