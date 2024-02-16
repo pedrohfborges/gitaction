3ca8843ac76e7b44d16100684cd7d789eb787803
Pedro Henrique Ferreira Borges <pedrohfborges4@gmail.com>
2024-02-16 08:08:22 -0300

    Atualizando rede

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,16 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "instancia" {
   ami           = var.instanceami
   instance_type = var.instancetype
   subnet_id     = aws_subnet.public_subnet.id
+  user_data     = <<-EOF
+                #!bin/bash
+                sudo apt update
+                sudo apt install apache2
+                EOF
+
+ tags {
+
+    Name = "Primeira Instancia"
+ }
 
 }
