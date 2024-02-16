d76538f98be4510dd07699144f5c81ba586181b3
Pedro Henrique Ferreira Borges <pedrohfborges4@gmail.com>
2024-02-16 07:48:44 -0300

    Atualizando rede

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 ####NET CONFIGURATION
 
 variable vpc {
-  default     = "10.10.0.0/16"
+  default     = "192.168.0.0/16"
   description = "Set VCN IP"
 }
 
 variable netpublic {
-  default     = "10.10.1.0/24"
+  default     = "192.168.30.0/24"
   description = "Set Subnet Public"
 }
 
 variable netprivate {
-  default     = "10.10.2.0/24"
+  default     = "192.168.40.0/24"
   description = "Set subnet Private"
 }
 
