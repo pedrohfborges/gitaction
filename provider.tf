f99d8bc294ef304f65421724fa616542d9746669
Pedro Henrique Ferreira Borges <111831621+pedrohfborges@users.noreply.github.com>
2024-02-19 22:19:18 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region  = "us-east-1"
+  region  = "us-east-2"
   version = "~> 2.0"
 }
 
 terraform {
   backend "s3" {
-    bucket  = "marina-bucket"
+    bucket  = "pedrobucket26"
     key     = "terraform-test.tfstate"
-    region  = "us-east-1"
+    region  = "us-east-2"
     encrypt = true
   }
 
